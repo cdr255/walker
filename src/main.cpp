2c48ad7a3824d7bb23d1b61c6ffbd39f069cecb6
Christopher Rodriguez <cdr255@gmail.com>
2014-11-23 06:40:28 -0500

    Added main loop and event handling

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,23 @@
 int main()
 {
   sf::Window AppWindow(sf::VideoMode(800, 600), "My Window");
+  
+
+  // Main Loop
+  while(AppWindow.isOpen())
+    {
+      // Event Handling
+      sf::Event event;
+      while(AppWindow.pollEvent(event))
+	{
+	  if(event.type == sf::Event::Closed)
+	    {
+	      // Close
+	      AppWindow.close();
+	    }
+	}
+    }
+
+
   return 0;
 }
