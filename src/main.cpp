d3c3c6b239764d943b86b4271cb3eb3a22f379be
Christopher Rodriguez <cdr255@gmail.com>
2014-11-23 07:01:23 -0500

    Added simple player class, drew box to screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+const int TILESIZE=32;
+
+class Character {
+public:
+  //sf::RectangleShape sprite;
+  int x, y, size;
+  sf::RectangleShape hit_box;
+  Character(int, int);
+};
+
+
+Character::Character(int a, int b) {
+  x = a;
+  y = b;
+  size = TILESIZE;
+  hit_box.setSize(sf::Vector2f(size, size));
+  hit_box.setPosition(x, y);
+}
+  
+
+
 
 int main()
 {
-  sf::Window AppWindow(sf::VideoMode(800, 600), "My Window");
+  sf::RenderWindow AppWindow(sf::VideoMode(800, 600), "My Window");
+  // sf::RectangleShape player(sf::Vector2f(120,50));
+  Character player(10, 10);
   
-
   // Main Loop
   while(AppWindow.isOpen())
     {
@@ -18,6 +41,11 @@ int main()
 	      AppWindow.close();
 	    }
 	}
+      
+      //Update Window
+      AppWindow.clear(sf::Color::Black);
+      AppWindow.draw(player.hit_box);
+      AppWindow.display();
     }
 
 
